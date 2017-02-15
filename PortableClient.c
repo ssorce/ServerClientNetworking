9f086d2eb2b29bf46fa796640080031c165f40cd
root <ssorce@email.arizona.edu>
2017-02-15 11:01:15 -0700

    Loops added to Client and Server (breaks on '\n')

--- PortableClient.c
+++ PortableClient.c
@@ -10,7 +10,15 @@ int main(){
 	char message[256];
 	cpRecv(socket,message,256);
 	len = strlen(message);
-	printf("Server says %s_%d\n", message, len);
+	while(cpCheckError(socket) == 0)
+	{
+		if(len == 0)
+			break;
+		printf("%d\n%s\n",len, message);
+		*message = '\0';
+		cpRecv(socket,message,256);
+		len = strlen(message);
+	}
 	cpClose(socket);
 	cpCloseNetwork();
 	return 0;
--- PortableServer.c
+++ PortableServer.c
@@ -8,8 +8,13 @@ int main(){
 	cpBind(socket);
 	cpListen(socket,5);
 	struct PortableSocket* client = cpAccept(socket);
-	scanf("%s", message);
-	cpSend(client, message,256);
+	while(fgets(message, 256, stdin) != NULL)
+	{
+		if(*message == '\n')
+			break;
+		cpSend(client, message, 256);
+	}
+	
 	cpClose(client);
 	cpClose(socket);
 	cpCloseNetwork();
