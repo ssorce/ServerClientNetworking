e497e41cd47df3a222212ef042e5436ce3dedbce
root <ssorce@email.arizona.edu>
2017-02-15 21:43:23 -0700

    Added args

--- PortableClient.c
+++ PortableClient.c
@@ -1,11 +1,12 @@
 #include "stdio.h"
 #include "PortableSocket.h"
 #include "string.h"
+#include "stdlib.h"
 
-int main(){
-	int len;
+int main(int argc, char *argv[]){
+	int len; // Will get rid of latter
 	cpOpenNetwork();
-	struct PortableSocket* socket = cpSocket(TCP,"localhost",10003);
+	struct PortableSocket* socket = cpSocket(TCP,argv[1],atoi(argv[2]));
 	cpConnect(socket);
 	char message[256];
 	cpRecv(socket,message,256);
@@ -14,7 +15,7 @@ int main(){
 	{
 		if(len == 0)
 			break;
-		printf("%d\n%s\n",len, message);
+		printf("%d\n%s\n",(len-1), message);
 		*message = '\0';
 		cpRecv(socket,message,256);
 		len = strlen(message);
--- PortableServer.c
+++ PortableServer.c
@@ -1,14 +1,15 @@
 #include "stdio.h"
+#include "stdlib.h"
 #include "PortableSocket.h"
 
-int main(){
+int main(int argc, char *argv[]){
 	char message[256];
 	cpOpenNetwork();
-	struct PortableSocket* socket = cpSocket(TCP,"localhost",10003);
+	struct PortableSocket* socket = cpSocket(TCP,"localhost", atoi(argv[1]));
 	cpBind(socket);
 	cpListen(socket,5);
 	struct PortableSocket* client = cpAccept(socket);
-	while(fgets(message, 256, stdin) != NULL)
+	while(fgets(message, 256, stdin) != NULL && cpCheckError(socket) == 0)
 	{
 		if(*message == '\n')
 			break;
