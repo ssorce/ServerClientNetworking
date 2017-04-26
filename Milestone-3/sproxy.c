92322c51972667417671a49f92c1a07308ad231c
Jonathon Davis <jonathondevindavis@gmail.com>
2017-04-25 18:57:17 -0700

    listen acutally added

--- Milestone-3/sproxy.c
+++ Milestone-3/sproxy.c
@@ -267,7 +267,7 @@ int main(int argc, char *argv[])
       if (mode == 1)
         printf("Server connection timed out\n");
       cpClose(clientProxy);
-      cpListen(clientAcceptor,1);
+      cpListen(clientAcceptor,5);
       clientConnected = 0;
       int socketN[] = {telnetSocket->socket, clientAcceptor->socket};
       n = getN(socketN, 2);
